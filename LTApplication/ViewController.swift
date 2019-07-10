d1138a099b8bfa47d3c1ac3417f79445d6c61b00
Branislav Manojlovic <branislav.manojlovic@zesium.com>
2019-07-10 11:01:22 +0200

    Initial Commit

---
+++ LTApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LTApplication
+//
+//  Created by Branislav Manojlovic on 4/19/1398 AP.
+//  Copyright © 1398 Branislav Manojlovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
